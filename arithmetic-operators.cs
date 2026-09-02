f7d78470e3f9ed785a8cd48646636af7231b932c
Sofia Uhrbom <sofia.uhrbom@gmail.com>
2026-09-02 09:44:26 +0200

    Added change-values-of-variables.

---
+++ arithmetic-operators.cs
@@ -0,0 +1,8 @@
+double a = 10;
+double b = 5;
+
+Console.WriteLine($"{a} + {b} = {a+b}");
+Console.WriteLine($"{a} + {b} = {a+b}");
+Console.WriteLine($"{a} + {b} = {a+b}");
+Console.WriteLine($"{a} + {b} = {a+b}");
+Console.WriteLine($"{a} + {b} = {a+b}");
---
+++ change-values-of-variables.cs
@@ -0,0 +1,8 @@
+int counter =1;
+Console.WriteLine(counter);
+
+counter = counter + 1;
+Console.WriteLine(counter);
+
+counter += 1;
+Console.WriteLine(counter);
