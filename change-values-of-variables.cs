5bf47c564d04cd29df323139d494fb7edb6cf942
Sofia Uhrbom <sofia.uhrbom@gmail.com>
2026-09-02 10:15:21 +0200

    Added more arithmetic code.

--- change-values-of-variables.cs
+++ change-values-of-variables.cs
@@ -1,8 +1,23 @@
-int counter =1;
+int counter = 1;
 Console.WriteLine(counter);
 
 counter = counter + 1;
 Console.WriteLine(counter);
 
 counter += 1;
+Console.WriteLine(counter);
+
+counter++;
+Console.WriteLine(counter);
+
+counter = 10;
+Console.WriteLine(counter);
+
+counter = counter - 1;
+Console.WriteLine(counter);
+
+counter -= 1;
+Console.WriteLine(counter);
+
+counter--;
 Console.WriteLine(counter);
